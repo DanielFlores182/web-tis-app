28344c40f06a7e7ea055fa6683a876f3779f078b
Andrea <andrea.calle.rojas666@gmail.com>
2024-09-29 21:46:01 -0400

    Cambio de estetica

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -37,22 +37,25 @@ function Login() {
 
   return (
     <div className="login-container">
+      <h1 className="login-title">Iniciar Sesión</h1>
+      <p className="login-subtitle">Inicia sesión en tu cuenta para continuar
+      </p>
       <div className="bubble">
-        <h1 className="login-title">Iniciar Sesión</h1>
         <form className="login-form" onSubmit={handleSubmit}>
           <div className="form-group">
-            <label htmlFor="username">Usuario</label>
+            <label htmlFor="username" className="form-label">Usuario</label>
             <input 
-              type="text" 
+              type="text"
               id="username" 
               placeholder="Ingrese su usuario" 
               value={username} 
               onChange={(e) => setUsername(e.target.value)} 
               required 
+              className="form-input"
             />
           </div>
           <div className="form-group">
-            <label htmlFor="password">Contraseña</label>
+            <label htmlFor="password" className="form-label">Contraseña</label>
             <input 
               type="password" 
               id="password" 
@@ -60,6 +63,7 @@ function Login() {
               value={clave} 
               onChange={(e) => setClave(e.target.value)} 
               required 
+              className="form-input"
             />
           </div>
           <button type="submit" className="login-button">Ingresar</button>
--- frontend/src/pages/Menu_est.js
+++ frontend/src/pages/Menu_est.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Menu_est.css';
+import logo from '../images/logo.png';
 
 function MenuEst() {
   return (
     <div className="menu-container">
       <aside className="sidebar">
+        <img src={logo} alt="Logo de la Empresa" className="header-logo"></img>
+        <h1 className="header-title">Estudiante</h1>
         <nav>
           <ul>
             <li><a href="/registrar_grupo">Registrarse en grupos</a></li>
+            <li><a href="/perfin">Tareas pendientes</a></li>
+            <li><a href="/perfin">Cronograma de actividades</a></li>
+            <li><a href="/perfin">Historial de evaluaciones</a></li>
+            <li><a href="/perfin">Ver grupo</a></li>
             <li><a href="/perfin">Perfil</a></li>
+            <li><a href="/perfin">Darse de baja</a></li>
             <li><a href="/est_config">Configuraciones</a></li>
             <li><a href="/">Cerrar Sesion</a></li>
           </ul>
