0794da397dd72704516ac25adc743318a761d591
DanielFlores182 <danmarceflores@gmail.com>
2024-09-29 23:47:43 -0400

    add registrar por lotes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Menuest from './pages/Menu_est.js';
 import RegEstInd from './pages/registro_est_ind.js';
 import GroupView from './pages/Grupos/GruposView.js';
 import GruposMod from './pages/Grupos/GruposMod.js';
+import RegEstLot from './pages/reg_est_lot.js'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/menu_doc" element={<Menudoc />} />
         <Route path="/menu_est" element={<Menuest />} />
         <Route path="/registro_est_ind" element={<RegEstInd />} />
+        <Route path="/registro_est_lot" element={<RegEstLot />} />
         <Route path="/registrar_grupo" element={<GroupView />} />
         <Route path="/modificar_grupo" element={<GruposMod />} />
       </Routes>
