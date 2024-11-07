f01d07ca4b3acb3f02bea3ec2a82078d9ba67559
DanielFlores182 <danmarceflores@gmail.com>
2024-11-06 22:45:59 -0400

    FIX JUAN MERGE

--- backend/add_acta_semanal.php
+++ backend/add_acta_semanal.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://webtismanager.netlify.app");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
